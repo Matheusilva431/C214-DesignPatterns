7c1a1f0840ac7b7e776feec287b3aa102b970be7
Matheus Chagas <matheusilva431+github@gmail.com>
2022-11-30 12:45:48 -0300

    Implementando o algoritmo de Heap Sort

---
+++ ordenacao/src/main/java/ordena/impl/ordenaHeapSort.java
@@ -0,0 +1,35 @@
+package ordena.impl;
+
+import iOrdenar;
+
+public class ordenaHeapSort implements iOrdenar{
+    @Override
+    public int[] ordena(int[] vetor) {
+        int size = vetor.length;
+        for (int i = size / 2 - 1; i >= 0; i--)
+            heapify(vetor, size, i);
+        for (int i = size - 1; i >= 0; i--) {
+            int x = vetor[0];
+            vetor[0] = vetor[i];
+            vetor[i] = x;
+            heapify(vetor, i, 0);
+        }
+        return vetor;
+    }
+
+    public void heapify(int array[], int SizeofHeap, int i) {
+        int largestelement = i;
+        int leftChild  = 2*i + 1;
+        int rightChild  = 2*i + 2;
+        if (leftChild  < SizeofHeap && array[leftChild ] > array[largestelement])
+            largestelement = leftChild ;
+        if (rightChild  < SizeofHeap && array[rightChild ] > array[largestelement])
+            largestelement = rightChild ;
+        if (largestelement != i) {
+            int temp = array[i];
+            array[i] = array[largestelement];
+            array[largestelement] = temp;
+            heapify(array, SizeofHeap, largestelement);
+        }
+    }
+}
